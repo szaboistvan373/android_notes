60470d1104396e341c677eda0ab3df469f03cd4a
Istvan Szabo <istvan@istvans-macbook-pro-1.home>
2021-06-11 22:11:16 +0200

    Add project files

---
+++ app/src/main/java/com/example/android/trackmysleepquality/note/NotesFragment.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.note
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.databinding.FragmentNotesBinding
+
+
+class NotesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentNotesBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_notes, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        // Create an instance of the ViewModel Factory.
+        val viewModelFactory = NotesViewModelFactory()
+
+        // Get a reference to the ViewModel associated with this fragment.
+        val notesViewModel =
+                ViewModelProvider(
+                        this, viewModelFactory).get(NotesViewModel::class.java)
+
+        // To use the View Model with data binding, you have to explicitly
+        // give the binding object a reference to it.
+        binding.notesViewModel = notesViewModel
+
+        // binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
+
+        val manager = LinearLayoutManager(activity)
+
+        binding.notesList.layoutManager = manager
+
+        val adapter = NotesAdapter(NotesListener { noteId ->
+            println(showdialog(activity))
+            println(noteId)
+        })
+        binding.notesList.adapter = adapter
+
+        notesViewModel.notes.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.addHeaderAndSubmitList(it)
+            }
+        })
+
+        return binding.root
+    }
+}
